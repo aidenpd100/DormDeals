a5b921631837cfa58edf1b5b52be2359980d7096
Aiden Deffner <aidenpd100@gmail.com>
2024-06-15 23:07:16 -0700

    added delete account button/functionality

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,35 +1,70 @@
-import { View, Text, Button, TouchableOpacity, ActivityIndicator } from 'react-native'
+import { View, Text, Button, TouchableOpacity, ActivityIndicator, Alert } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { styles } from '@/constants/Styles'
 import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context'
-import { FIREBASE_APP, FIREBASE_AUTH, FIREBASE_DB } from '@/FirebaseConfig'
+import { FIREBASE_APP, FIREBASE_AUTH, FIREBASE_DB, FIREBASE_STORAGE } from '@/FirebaseConfig'
 import { User } from 'firebase/auth'
-import { Firestore, collection, doc, getDoc, getDocFromServer, getFirestore } from 'firebase/firestore'
+import { Firestore, collection, deleteDoc, doc, getDoc, getDocFromServer, getDocs, getFirestore, query, where } from 'firebase/firestore'
 import Header from '@/components/Header'
+import { ref } from 'firebase/storage'
 
 const Profile = ({ username }: { username: string }) => {
 
-    const db = getFirestore()
-    const [loading, setLoading] = useState(false)
+    const [loadingOut, setLoadingOut] = useState(false)
+    const [loadingDelete, setLoadingDelete] = useState(false)
 
-    const signOut = async () => {
+    const signOut = async (onlySignOut: boolean) => {
         try {
-            setLoading(true)
+            onlySignOut && setLoadingOut(true)
             await FIREBASE_AUTH.signOut()
         } catch (err) {
             console.log(err)
         } finally {
-            setLoading(false)
+            setLoadingOut(false)
+        }
+    }
+
+    const deletePrompt = async () => {
+        Alert.alert('Are you sure?', 'Your account will be permanently deleted', [
+            { text: 'Delete', onPress: () => deleteAccount() },
+            { text: 'Cancel', onPress: () => { } },
+        ])
+    }
+
+    const deleteAccount = async () => {
+        try {
+            setLoadingDelete(true)
+            const deletedUid = FIREBASE_AUTH.currentUser!.uid
+
+            signOut(false)
+            await deleteDoc(doc(FIREBASE_DB, 'users', deletedUid));
+
+            const q = query(collection(FIREBASE_DB, 'posts'), where('author_id', '==', deletedUid));
+            const querySnapshot = await getDocs(q);
+            querySnapshot.forEach((doc) => {
+                deleteDoc(doc.ref)
+            });
+
+            //TODO: delete messages? or just send a deleted account message?
+        } catch (err) {
+            console.log(err)
+        } finally {
+            setLoadingDelete(false)
         }
     }
 
     return (
         <View style={[styles.container, { paddingTop: useSafeAreaInsets().top }]}>
             <Header title='Profile' />
-            <Text style={{ color: '#fff', fontFamily: 'lex-xlight', marginTop: 30, fontSize: 20 }}>Username: {username}</Text>
-            <TouchableOpacity style={styles.button} onPress={signOut} activeOpacity={0.75}>
-                {loading ? <ActivityIndicator /> :
+            <Text style={{ color: '#fff', fontFamily: 'lex-xlight', marginTop: 30, marginBottom: 20, fontSize: 20 }}>Username: {username}</Text>
+            <TouchableOpacity style={[styles.button]} onPress={() => signOut(true)} activeOpacity={0.75}>
+                {loadingOut ? <ActivityIndicator /> :
                     <Text style={{ fontFamily: 'lex-sb', fontSize: 17 }}>Sign out</Text>
+                }
+            </TouchableOpacity>
+            <TouchableOpacity style={[styles.button, { backgroundColor: '#af0000' }]} onPress={deletePrompt} activeOpacity={0.75}>
+                {loadingDelete ? <ActivityIndicator /> :
+                    <Text style={{ fontFamily: 'lex-sb', fontSize: 17 }}>Delete account</Text>
                 }
             </TouchableOpacity>
         </View>
